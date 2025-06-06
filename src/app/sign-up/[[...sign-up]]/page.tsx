0f2fd4c2ed342ca8cfb727211d06daadcae2312b
Navneet Prajapati <navneetprajapati46@gmail.com>
2025-06-06 16:43:46 +0530

    fix: ui colors updated

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -5,14 +5,14 @@ export default function SignUpPage() {
   return (
     <div className="h-screen flex items-center">
       {/* Left Column - Auth Form */}
-      <div className="flex-1 flex items-center justify-center bg-white h-full">
+      <div className="flex-1 flex items-center justify-center h-full">
         <div className="w-full max-w-md flex items-center justify-center">
           <AuthForm mode="sign-up" />
         </div>
       </div>
 
       {/* Right Column - Hero Image & Testimonial */}
-      <HeroSection />
+      {/* <HeroSection /> */}
     </div>
   );
 }
