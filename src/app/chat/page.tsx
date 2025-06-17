9ff23cc9de84371a47e1e3eca5654d46bf5b3472
Navneet Prajapati <navneetprajapati46@gmail.com>
2025-06-17 05:48:21 +0530

    Squashed commit of the following:
    commit 92cb364b8c7016c62b02d9a4fe47e95c3c0aa04b
    Author: Navneet Prajapati <navneetprajapati46@gmail.com>
    Date:   Tue Jun 17 05:47:07 2025 +0530
    
        feat: create group UI updated - checkpoint
    
    commit e1173bb7f674138d15bac8406d876bfb49217daf
    Author: Navneet Prajapati <navneetprajapati46@gmail.com>
    Date:   Tue Jun 17 01:39:24 2025 +0530
    
        feat: added create group form - checkpoint
    
    commit a98f978153660d41dc56348652c57f528c87c0c2
    Author: Navneet Prajapati <navneetprajapati46@gmail.com>
    Date:   Tue Jun 17 00:50:52 2025 +0530
    
        installed heroUI

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,6 +1,6 @@
 export default function ChatPage() {
   return (
-    <div className="p-6">
+    <div className="p-6 w-screen h-screen">
       <h2 className="text-2xl font-bold mb-4">Chat</h2>
       {/* Placeholder for chat UI */}
       <div className="">Chat interface coming soon...</div>
--- src/app/group/page.tsx
+++ src/app/group/page.tsx
@@ -1,26 +1,9 @@
 export default function GroupPage() {
   return (
-    <div className="p-6">
-      <h2 className="text-2xl font-semibold mb-4">
-        Create or Join a Travel Group
-      </h2>
-      <form className="grid gap-4 max-w-md">
-        <input
-          type="text"
-          placeholder="Group Name"
-          className="border p-2 rounded bg-transparent"
-        />
-        <select
-          className="border p-2 rounded bg-transparent"
-          aria-label="Group Privacy Setting"
-        >
-          <option>Public</option>
-          <option>Private</option>
-        </select>
-        <button className="bg-primary text-white px-4 py-2 rounded">
-          Create
-        </button>
-      </form>
+    <div className="p-6 w-screen h-screen">
+      <h2 className="text-2xl font-bold mb-4">Group</h2>
+      {/* Placeholder for chat UI */}
+      <div className="">Group interface coming soon...</div>
     </div>
   );
 }
