71401e188d1c89f319342b9693e421615c943007
Navneet Prajapati <navneetprajapati46@gmail.com>
2025-06-18 07:51:39 +0530

    navbar wrapper no navbar pages added

--- src/components/layout/layout-wrapper.tsx
+++ src/components/layout/layout-wrapper.tsx
@@ -3,7 +3,14 @@
 import { usePathname } from "next/navigation";
 import Navbar from "@/components/Navbar";
 
-const HIDE_LAYOUT_ROUTES = ["/sign-in", "/sign-up"];
+const HIDE_LAYOUT_ROUTES = [
+  "/sign-in",
+  "/sign-up",
+  "/onboarding",
+  "/forgot-password",
+  "/verify-email",
+  "/sso-callback",
+];
 
 interface LayoutWrapperProps {
   children: React.ReactNode;
