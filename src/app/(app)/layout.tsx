97aaca1e2ef15112f535585ebf1b3ea197cf3b05
Navneet Prajapati <navneetprajapati46@gmail.com>
2025-06-24 04:04:19 +0530

    protected all the (app) routes

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,15 +1,18 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { SidebarWrapper } from "@/components/sidebar-wrapper";
 import LayoutWrapper from "@/components/layout/app-layout-wrapper";
+import ProtectedRoute from "@/components/protected-route";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider>
-      <SidebarWrapper />
-      <main className="flex-1 flex flex-col min-h-screen">
-        <LayoutWrapper>{children}</LayoutWrapper>
-        {/* {children} */}
-      </main>
-    </SidebarProvider>
+    <ProtectedRoute>
+      <SidebarProvider>
+        <SidebarWrapper />
+        <main className="flex-1 flex flex-col min-h-screen">
+          <LayoutWrapper>{children}</LayoutWrapper>
+          {/* {children} */}
+        </main>
+      </SidebarProvider>
+    </ProtectedRoute>
   );
 }
