a6b489eaff13fe8da1bec60d6537dd554b20a0f4
Navneet Prajapati <navneetprajapati46@gmail.com>
2025-12-10 01:48:41 +0530

    chore: bootstrap admin app structure

--- apps/admin/app/layout.tsx
+++ apps/admin/app/layout.tsx
@@ -19,16 +19,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className="min-h-screen bg-gray-50">{children}</body>
     </html>
   );
 }
