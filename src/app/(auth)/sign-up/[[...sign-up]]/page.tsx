cfea1d4991aab850dd8a97563aa5efacff7e5cab
Navneet Prajapati <navneetprajapati46@gmail.com>
2026-03-03 01:38:15 +0530

    feat: minor ui fixes for signin/signup pages

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -13,7 +13,7 @@ export default function SignUpPage() {
     <div className="h-screen flex items-center">
       {/* Left Column - Auth Form */}
       <div className="flex-1 flex items-center justify-center h-full">
-        <div className="w-full p-6 flex items-center justify-center">
+        <div className="w-full sm:p-6 p-3 flex items-center justify-center">
           <AuthForm mode="sign-up" />
         </div>
       </div>
