d81d2b3be620261082824477cb90be164ac3e0dc
Navneet Prajapati <navneetprajapati46@gmail.com>
2026-05-27 23:31:19 +0530

    fix: logs disabled for production env

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -7,6 +7,9 @@ const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
   async headers() {
     return [
       {
