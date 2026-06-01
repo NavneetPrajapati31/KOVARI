edd85bd8a3573f29d6d24de16715049608690d81
Navneet Prajapati <navneetprajapati46@gmail.com>
2026-06-02 01:44:25 +0530

    minor fix

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -375,12 +375,13 @@ export default async function middleware(req: NextRequest, evt: any) {
     const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
     const cspHeader = `
       default-src 'self';
-      script-src 'self' 'nonce-${nonce}' 'unsafe-inline' 'unsafe-eval' https://clerk.kovari.in https://*.clerk.accounts.dev https://va.vercel-scripts.com;
+      script-src 'self' 'nonce-${nonce}' 'unsafe-inline' 'unsafe-eval' https://clerk.kovari.in https://*.clerk.accounts.dev https://va.vercel-scripts.com https://challenges.cloudflare.com;
       style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://api.fontshare.com;
       img-src 'self' data: blob: https://res.cloudinary.com https://utfs.io https://img.clerk.com https://*.clerk.com https://images.clerk.dev https://*.googleusercontent.com https://*.supabase.co https://*.onrender.com;
       media-src 'self' data: blob: https://res.cloudinary.com https://*.onrender.com;
       font-src 'self' https://fonts.gstatic.com https://api.fontshare.com;
       connect-src 'self' https://*.supabase.co wss://*.supabase.co https://api.clerk.dev wss://kovari.in http://localhost:3005 ws://localhost:3005 https://vitals.vercel-insights.com https://api.cloudinary.com https://*.onrender.com wss://*.onrender.com https://*.clerk.accounts.dev https://clerk.kovari.in https://*.uploadthing.com;
+      frame-src 'self' https://challenges.cloudflare.com;
       frame-ancestors 'none';
       object-src 'none';
       base-uri 'self';
